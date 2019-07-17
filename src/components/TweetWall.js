4c12bd6daed01416a3d2e61e825f44d192056d09
Konstantinos Pediaditakis <pediadk@gmail.com>
2019-07-17 15:30:53 +0000

    Automatically backed up by Learn

--- src/components/TweetWall.js
+++ src/components/TweetWall.js
@@ -12,10 +12,13 @@ class TweetWall extends React.Component {
 
   // TODO: componentWillMount()
   componentWillMount() {
-    this.setState(tweets: this.props.newTweets);
+    this.setState({tweets: this.props.newTweets});
   }
   // TODO: shouldComponentUpdate()
   // TODO: componentWillReceiveProps()
+  componentWillReceiveProps(nextProps) {
+    this.setState({tweets: nextProps.newTweets.concat(this.state.tweets)});
+  }
 
   render() {
     const tweets = this.state.tweets.map((tweet, index) => <Tweet text={tweet.text} key={index} />);
